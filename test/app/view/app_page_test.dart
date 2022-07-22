3837ebeec0cb74255dcb994c9136cd5b3002e26b
chinjja <chinjja@gmail.com>
2022-07-22 15:15:21 +0900

    app page tests

---
+++ test/app/view/app_page_test.dart
@@ -0,0 +1,54 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:talk/app/app.dart';
+import 'package:talk/repos/repos.dart';
+
+import '../../mocks/mocks.dart';
+
+void main() {
+  group('App', () {
+    late AppBloc bloc;
+
+    setUp(() {
+      bloc = MockAppBloc();
+      when(() => bloc.state).thenReturn(AppState.unknown());
+    });
+
+    test('initial state is AppState.unknown', () {
+      final authRepository = MockAuthRepository();
+      expect(
+        AppBloc(authRepository: authRepository).state,
+        AppState.unknown(),
+      );
+    });
+
+    group('calls', () {
+      //
+    });
+
+    group('renders', () {
+      testWidgets('when authentication is emitted then show AppView',
+          (tester) async {
+        final authRepository = MockAuthRepository();
+        final chatRepository = MockChatRepository();
+        when(() => authRepository.onAuthChanged).thenAnswer(
+          (_) => Stream.fromIterable([
+            Authentication(
+              emailVerified: true,
+              principal: User(
+                username: 'user',
+              ),
+            )
+          ]),
+        );
+        await tester.pumpWidget(App(
+          authRepository: authRepository,
+          chatRepository: chatRepository,
+        ));
+        expect(find.byType(AppView), findsOneWidget);
+      });
+    });
+  });
+}
--- test/mocks/mocks.dart
+++ test/mocks/mocks.dart
@@ -2,10 +2,13 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:talk/app/app.dart';
 import 'package:talk/pages/register/register.dart';
 import 'package:talk/repos/repos.dart';
 
 class MockAuthRepository extends Mock implements AuthRepository {}
+
+class MockChatRepository extends Mock implements ChatRepository {}
 
 class MockAuthProvider extends Mock implements AuthProvider {}
 
@@ -24,6 +27,8 @@ class MockChatListenProvider extends Mock implements ChatListenProvider {}
 class MockDio extends Mock implements Dio {}
 
 class MockFlutterSecureStorage extends Mock implements FlutterSecureStorage {}
+
+class MockAppBloc extends MockBloc<AppEvent, AppState> implements AppBloc {}
 
 class MockRegisterBloc extends MockBloc<RegisterEvent, RegisterState>
     implements RegisterBloc {}
